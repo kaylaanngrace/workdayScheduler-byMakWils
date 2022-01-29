5299fc85331a0daaecba9a9c71a8aed6457cc61b
MakWils <kaylaanngrace@gmail.comgit remote add origin git@github.com:kaylaanngrace/horiseon-optimize-accessibility.git>
2022-01-29 02:32:51 -0600

    added setTimeout() to the DateTime

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,14 +3,22 @@ var hourEl = document.querySelector("#hour");
 var detailsEl = document.querySelector("#details");
 var saveButtonEl = document.querySelector("#saveBtn");
 
-var DateTime = luxon.DateTime;
+function updateDateTime() {
+    var DateTime = luxon.DateTime;
 
-today = DateTime.local();
-currentDayEl.textContent = today.toLocaleString(DateTime.DATE_HUGE);
+    today = DateTime.local();
+    currentDayEl.textContent = today.toLocaleString(DateTime.DATE_HUGE);
 
-var currentTimeEl = document.createElement("p");
-currentTimeEl.textContent = today.toLocaleString(DateTime.TIME_WITH_SECONDS);
-currentTimeEl.setAttribute("style", "padding-top:10px;")
-currentDayEl.appendChild(currentTimeEl);
+    var currentTimeEl = document.createElement("p");
+    currentTimeEl.textContent = today.toLocaleString(DateTime.TIME_WITH_SECONDS);
+    currentTimeEl.setAttribute("style", "padding-top:10px;")
+    currentDayEl.appendChild(currentTimeEl);
+
+    setTimeout(updateDateTime,1000);
+}
+updateDateTime();
+
+
+
 
 
