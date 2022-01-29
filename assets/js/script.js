e1f86f3ad1629560835dfb3969613b7c30eef3cc
MakWils <kaylaanngrace@gmail.comgit remote add origin git@github.com:kaylaanngrace/horiseon-optimize-accessibility.git>
2022-01-29 03:20:46 -0600

    added hours 8am - 8pm in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,9 @@ var hourEl = document.querySelector("#hour");
 var detailsEl = document.querySelector("#details");
 var saveButtonEl = document.querySelector("#saveBtn");
 
+var DateTime = luxon.DateTime;
 function updateDateTime() {
-    var DateTime = luxon.DateTime;
-
+    
     today = DateTime.local();
     currentDayEl.textContent = today.toLocaleString(DateTime.DATE_HUGE);
 
@@ -17,6 +17,24 @@ function updateDateTime() {
     setTimeout(updateDateTime,1000);
 }
 updateDateTime();
+
+// hours
+var hours = [
+    hour8 = {hour: '8'},
+    hour9 = {hour: '9'},
+    hour10 = {hour: '10'},
+    hour11 = {hour: '11'},
+    hour12 = {hour: '12'},
+    hour13 = {hour: '13'}, 
+    hour14 = {hour: '14'}, 
+    hour15 = {hour: '15'}, 
+    hour16 = {hour: '16'}, 
+    hour17 = {hour: '17'},
+    hour18 = {hour: '18'}, 
+    hour19 = {hour: '19'}, 
+    hour20 = {hour: '20'}, 
+];
+
 
 
 
