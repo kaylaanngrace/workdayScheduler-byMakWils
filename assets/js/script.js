47514d18dc4e6d28bc6da9b6b8cc6261f86b31a0
MakWils <kaylaanngrace@gmail.comgit remote add origin git@github.com:kaylaanngrace/horiseon-optimize-accessibility.git>
2022-01-29 02:26:15 -0600

    added current date and time

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,15 @@ var currentDayEl = document.querySelector("#currentDay");
 var hourEl = document.querySelector("#hour");
 var detailsEl = document.querySelector("#details");
 var saveButtonEl = document.querySelector("#saveBtn");
+
+var DateTime = luxon.DateTime;
+
+today = DateTime.local();
+currentDayEl.textContent = today.toLocaleString(DateTime.DATE_HUGE);
+
+var currentTimeEl = document.createElement("p");
+currentTimeEl.textContent = today.toLocaleString(DateTime.TIME_WITH_SECONDS);
+currentTimeEl.setAttribute("style", "padding-top:10px;")
+currentDayEl.appendChild(currentTimeEl);
+
+
